b7414aeaf598621622fe8351cdb8eadbac96068b
Deniz <auo@tuta.io>
2025-07-28 15:08:01 +0200

    corrected img path

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 function Header() {
     return (
         <header>
-            <img src="../public/Rectangle 90.png" />
+            <img src="/Rectangle 90.png" />
             <h1>Laura Smith</h1>
             <h2>Front-End Developer</h2>
             <a href="#">laurasmith.website</a>
